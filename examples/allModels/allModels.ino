6304996851ae1caf092df532730933e7f18f4003
Jeremy Ellis <keyfreemusic@gmail.com>
2024-07-23 10:12:07 -0700

    Update allModels.ino

--- examples/allModels/allModels.ino
+++ examples/allModels/allModels.ino
@@ -28,7 +28,7 @@
 #include <RocksettaTinyML.h>
 
 // sine_model.h contains the array you exported from the previous step with xxd or tinymlgen
-#include "xor_model.h"
+#include "xOr_model.h"
 
 #define NUMBER_OF_INPUTS 25
 #define NUMBER_OF_OUTPUTS 2
